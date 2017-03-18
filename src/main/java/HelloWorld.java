1ff81ade3454c652f35dddb7037e8919a99e774b
test automation trainings <test.automation.office@gmail.com>
2017-03-18 16:45:33 +0100

    hallo world 1

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Squall on 18.03.2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hallo world!");
+    }
+}
--- src/main/java/Test.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Squall on 18.03.2017.
- */
-public class Test {
-}
